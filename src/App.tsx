2dd460fc25d4e059988f0ee375051f0d6041279a
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-07-19 04:11:01 +0900

    Route追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
 import Agreements from "./pages/agreements";
 import Vote from "./pages/vote";
+import Callback from "./pages/callback";
 
 function App() {
   return(
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/agreements" element={<Agreements />} />
       <Route path="/vote" element={<Vote />} />
+      <Route path="/callback" element={<Callback />} />
     </Routes>
   )
 }
