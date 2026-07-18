eafc2e57570402fcbfe0c5f3e5b0fac6c83bb1ae
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-07-19 01:15:28 +0900

    スマホレスポンシブ

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react"
+
 function Header() {
+  const [open, setOpen] = useState(false);
   return (
     <header className="fixed top-0 left-0 z-50 w-full border-b-2 border-black/10 bg-linear-to-r from-[#3D0E66] to-[#2C75FF] text-white">
       <div className="absolute inset-0 pointer-events-none bg-[radial-gradient(circle_at_top_left,rgba(255,255,255,0.18),transparent_35%),radial-gradient(circle_at_bottom_right,rgba(255,255,255,0.12),transparent_40%)]" />
       <div className="relative z-10 max-w-6xl mx-auto py-6 px-4 flex items-center justify-between">
         <p className="font-semibold text-[20px]">Egg Server Bot</p>
-        <nav>
+        <nav className="hidden md:block">
           <ul className="flex items-center gap-8 text-slate-300">
             <li>
               <a href="#" className="hover:text-white transition-colors duration-200">
@@ -28,6 +31,37 @@ function Header() {
             </li>
           </ul>
         </nav>
+
+        <button className="md:hidden text-2xl"
+          onClick={() => setOpen(!open)}>≡</button>
+
+        <div className={`md:hidden overflow-hidden transition-all duration-300 
+        ${open ? "max-h-64 opacity-100" : "max-h-0 opacity-0"}`}>
+          <nav className="border-t border-white/20 px-6 py-4">
+            <ul className="flex flex-col gap-4 text-slate-500">
+              <li>
+                <a href="#" className="hover:text-whitet transition-colors duration-200">
+                  ホーム
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-whitet transition-colors duration-200">
+                  コマンド
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-whitet transition-colors duration-200">
+                  機能
+                </a>
+              </li>
+              <li>
+                <a href="/agreements" className="hover:text-whitet transition-colors duration-200">
+                  利用規約
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   )
