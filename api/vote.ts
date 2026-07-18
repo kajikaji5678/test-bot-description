e1c9c5bf35548664b1f211bd8526b20c698c447f
kajikaji5678 <bestlife.fukuda.kajito@gmail.com>
2026-07-19 03:37:07 +0900

    テスト

--- api/vote.ts
+++ api/vote.ts
@@ -1,11 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
+import type {VercelRequest, VercelResponse} from "@vercel/node";
 
 const supabase = createClient(
     process.env.SUPABASE_URL!,
     process.env.SUPABASE_SECRET_KEY!,
 );
 
-export default async function handler(req: any, res:any) {
+export default async function handler
+(req: VercelRequest, res:VercelResponse) {
     if (req.method !== "POST") {
         return res.status(405).json({
             message: "Method Not Allowed",
